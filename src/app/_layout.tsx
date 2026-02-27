ef07adc708b5c76009288b77ce9d1fc679249e64
ceejaymar <martinezcj2@gmail.com>
2026-02-26 20:35:17 -0500

    added sentry

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import * as Sentry from '@sentry/react-native';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -8,11 +9,30 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { SystemBars } from 'react-native-edge-to-edge';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useUnistyles } from 'react-native-unistyles';
 import migrations from '@/drizzle/migrations';
 
 import { db } from '@/src/db/client';
 import '@/src/i18n/index';
-import { useUnistyles } from 'react-native-unistyles';
+
+Sentry.init({
+  dsn: 'https://06641ea19a9965be5f3dcbdb6b3d04e5@o4510953598222336.ingest.us.sentry.io/4510953601236992',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -24,7 +44,7 @@ export const unstable_settings = {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     Fraunces: require('../assets/fonts/Fraunces-VariableFont.ttf'),
@@ -74,3 +94,5 @@ function RootLayoutNav() {
     </>
   );
 }
+
+export default Sentry.wrap(RootLayout);
