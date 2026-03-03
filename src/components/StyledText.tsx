7c8660e3a1873ffeb5639b94091a46d6fc6fec1b
ceejaymar <martinezcj2@gmail.com>
2026-03-02 22:17:00 -0500

    remove unused files

--- src/components/StyledText.tsx
+++
@@ -1,6 +0,0 @@
-import type { TextProps } from 'react-native';
-import { ThemedText } from './themed-text';
-
-export function MonoText(props: TextProps) {
-  return <ThemedText {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
-}
